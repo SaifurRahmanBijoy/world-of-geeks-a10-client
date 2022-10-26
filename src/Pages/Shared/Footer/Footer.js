07553d66244b1802e763668e209e319a3d7eac84
SaifurRahmanBijoy <saifurrahmanbijoy1@gmail.com>
2022-10-26 23:06:04 +0600

    header,footer modified

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-gray-200 mt-4">
-        <div className="bottom-0 w-1/4 mx-auto">
+        <div className="">
       <p className="text-blue-800 font-thin text-sm font-mono py-2 text-center">
         Copyright goes to{" "}
         <a href="https://www.facebook.com/B1JOY">Saifur Rahman</a>
